093037bcf85d8583ddb8ce193da826eeb233623b
Albin <albinronnkvist@protonmail.com>
2024-11-07 15:20:42 +0100

    Add deterministic function example

---
+++ LearningObjectives/functional/deterministic-function.js
@@ -0,0 +1,10 @@
+/*
+ * A deterministic function is a function that, given the same input, always returns the same output.
+ */
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3)); // Output: 5
+console.log(add(2, 3)); // Output: 5
